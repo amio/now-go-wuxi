b7c630ba8be1c9aed335563a46d24f8a63adfb74
Amio <amio.cn@gmail.com>
2016-09-22 11:22:11 +0800

    Keep self alive.

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const go = require('now-go')
-const configLocation = './config.json' // local file or remote url
+const fetch = require('node-fetch')
+const configLocation = 'https://amio.github.io/now-go-wuxi/config.json'
 
 go(configLocation)
+
+function ping () {
+  return fetch(configLocation)
+  .then(res => res.json())
+  .then(cfg => {
+    if (cfg.deployment) {
+      return fetch(cfg.deployment) // ping deployment url to keep alive
+    }
+  })
+}
+
+setInterval(ping, 60 * 60 * 1000)
